21876426fe8a733f308d39b4f9c38e2fff2c90cc
moradalkhatib <muradalkhateb@icloud.com>
2021-08-21 17:44:35 +0300

    newRcipesComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-
+import NewRecipes from "./components/NewRecipes";
 export default class App extends Component {
   render() {
-    return <div>Hello from initial commit</div>;
+    return <div><NewRecipes/></div>;
   }
 }
