a3f46f9684269fa171fb26db91399873eb77c2bc
Hiba-Almade <hibaalmade998@gmail.com>
2021-08-21 17:21:19 +0300

    footer design done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
   render() {
-    return <div>Hello from initial commit</div>;
+    return (
+    
+    
+    <Footer/>
+      );
   }
 }
